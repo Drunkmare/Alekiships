c53dadbbc5243c66129fbed816492d7fa844af8f
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-02-01 00:51:18 -0800

    Our "simple block menu compartments" use the new interface via an abstract implementation SimpleBlockMenuCompartmentEntity. This class also provides some common functionality

--- src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/CraftingTableCompartment.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/CraftingTableCompartment.java
@@ -1,26 +1,23 @@
 package com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.vanilla;
 
 import com.alekiponi.alekiships.common.entity.vehiclehelper.CompartmentType;
-import com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.BlockCompartmentEntity;
-import net.minecraft.core.BlockPos;
+import com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.SimpleBlockMenuCompartmentEntity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stat;
 import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
-import java.util.function.BiFunction;
+public class CraftingTableCompartment extends SimpleBlockMenuCompartmentEntity {
 
-public class CraftingTableCompartment extends BlockCompartmentEntity implements MenuProvider {
+    private static final Component CONTAINER_TITLE = Component.translatable("container.crafting");
 
     public CraftingTableCompartment(final EntityType<? extends CraftingTableCompartment> entityType,
             final Level level) {
@@ -33,27 +30,23 @@ public class CraftingTableCompartment extends BlockCompartmentEntity implements 
     }
 
     @Override
-    public InteractionResult interact(final Player player, final InteractionHand hand) {
-        player.openMenu(this);
-        player.awardStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
-        return InteractionResult.SUCCESS;
+    public @Nullable AbstractContainerMenu createMenu(final int id, final Inventory playerInventory,
+            final Player player) {
+        return new CraftingMenu(id, playerInventory, this.getContainerLevelAccess()) {
+            @Override
+            public boolean stillValid(final Player player) {
+                return CraftingTableCompartment.this.stillValid(player);
+            }
+        };
     }
 
     @Override
-    public @Nullable AbstractContainerMenu createMenu(final int id, final Inventory playerInventory,
-            final Player player) {
-        return new CraftingMenu(id, playerInventory, new ContainerLevelAccess() {
-            @Override
-            public <T> Optional<T> evaluate(final BiFunction<Level, BlockPos, T> function) {
-                return Optional.of(function.apply(CraftingTableCompartment.this.level(),
-                        CraftingTableCompartment.this.blockPosition()));
-            }
-        }) {
-            @Override
-            public boolean stillValid(final Player player) {
-                final BlockPos blockPos = CraftingTableCompartment.this.blockPosition();
-                return player.distanceToSqr(blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5) <= 64;
-            }
-        };
+    protected Stat<ResourceLocation> getInteractionStat() {
+        return Stats.CUSTOM.get(Stats.INTERACT_WITH_CRAFTING_TABLE);
+    }
+
+    @Override
+    protected Component getContainerTitle() {
+        return CONTAINER_TITLE;
     }
 }
--- src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/GrindstoneCompartmentEntity.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/GrindstoneCompartmentEntity.java
@@ -1,17 +1,15 @@
 package com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.vanilla;
 
 import com.alekiponi.alekiships.common.entity.vehiclehelper.CompartmentType;
-import com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.BlockCompartmentEntity;
-import net.minecraft.core.BlockPos;
+import com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.SimpleBlockMenuCompartmentEntity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stat;
 import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.GrindstoneMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -19,10 +17,9 @@ import net.minecraft.world.level.block.GrindstoneBlock;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
-import java.util.function.BiFunction;
+public class GrindstoneCompartmentEntity extends SimpleBlockMenuCompartmentEntity {
 
-public class GrindstoneCompartmentEntity extends BlockCompartmentEntity implements MenuProvider {
+    private static final Component CONTAINER_TITLE = Component.translatable("container.grindstone_title");
 
     public GrindstoneCompartmentEntity(final EntityType<? extends GrindstoneCompartmentEntity> entityType,
             final Level level) {
@@ -35,28 +32,24 @@ public class GrindstoneCompartmentEntity extends BlockCompartmentEntity implemen
         this.setDisplayBlockState(this.getDisplayBlockState().setValue(GrindstoneBlock.FACE, AttachFace.FLOOR));
     }
 
+    @Nullable
     @Override
-    public InteractionResult interact(final Player player, final InteractionHand hand) {
-        player.openMenu(this);
-        player.awardStat(Stats.INTERACT_WITH_GRINDSTONE);
-        return InteractionResult.SUCCESS;
+    public AbstractContainerMenu createMenu(final int id, final Inventory playerInventory, final Player player) {
+        return new GrindstoneMenu(id, playerInventory, this.getContainerLevelAccess()) {
+            @Override
+            public boolean stillValid(final Player player) {
+                return GrindstoneCompartmentEntity.this.stillValid(player);
+            }
+        };
     }
 
     @Override
-    public @Nullable AbstractContainerMenu createMenu(final int id, final Inventory playerInventory,
-            final Player player) {
-        return new GrindstoneMenu(id, playerInventory, new ContainerLevelAccess() {
-            @Override
-            public <T> Optional<T> evaluate(final BiFunction<Level, BlockPos, T> function) {
-                return Optional.of(function.apply(GrindstoneCompartmentEntity.this.level(),
-                        GrindstoneCompartmentEntity.this.blockPosition()));
-            }
-        }) {
-            @Override
-            public boolean stillValid(final Player player) {
-                final BlockPos blockPos = GrindstoneCompartmentEntity.this.blockPosition();
-                return player.distanceToSqr(blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5) <= 64;
-            }
-        };
+    protected Stat<ResourceLocation> getInteractionStat() {
+        return Stats.CUSTOM.get(Stats.INTERACT_WITH_GRINDSTONE);
+    }
+
+    @Override
+    protected Component getContainerTitle() {
+        return CONTAINER_TITLE;
     }
 }
