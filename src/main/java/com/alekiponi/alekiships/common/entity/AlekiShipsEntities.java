12734c1c83c07f3afc6cd1449035bacb9d489f9d
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-02-22 04:11:06 -0800

    Use Items.* instead of Blocks.* for our compartment predicates

--- src/main/java/com/alekiponi/alekiships/common/entity/AlekiShipsEntities.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/AlekiShipsEntities.java
@@ -15,7 +15,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -57,16 +57,16 @@ public final class AlekiShipsEntities {
     public static final RegistryObject<CompartmentType<BarrelCompartmentEntity>> BARREL_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_barrel",
             CompartmentType.Builder.of(BarrelCompartmentEntity::new, BarrelCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.BARREL.asItem()));
+            itemStack -> itemStack.is(Items.BARREL));
 
     public static final RegistryObject<CompartmentType<ChestCompartmentEntity>> CHEST_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_chest", CompartmentType.Builder.of(ChestCompartmentEntity::new, ChestCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.CHEST.asItem()));
+            itemStack -> itemStack.is(Items.CHEST));
 
     public static final RegistryObject<CompartmentType<EnderChestCompartmentEntity>> ENDER_CHEST_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_ender_chest",
             CompartmentType.Builder.of(EnderChestCompartmentEntity::new, EnderChestCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.ENDER_CHEST.asItem()));
+            itemStack -> itemStack.is(Items.ENDER_CHEST));
 
     public static final RegistryObject<CompartmentType<ShulkerBoxCompartmentEntity>> SHULKER_BOX_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_shulker_box",
@@ -76,22 +76,22 @@ public final class AlekiShipsEntities {
     public static final RegistryObject<CompartmentType<FurnaceCompartmentEntity>> FURNACE_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_furnace",
             CompartmentType.Builder.of(FurnaceCompartmentEntity::new, FurnaceCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.FURNACE.asItem()));
+            itemStack -> itemStack.is(Items.FURNACE));
 
     public static final RegistryObject<CompartmentType<BlastFurnaceCompartmentEntity>> BLAST_FURNACE_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_blast_furnace",
             CompartmentType.Builder.of(BlastFurnaceCompartmentEntity::new, BlastFurnaceCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.BLAST_FURNACE.asItem()));
+            itemStack -> itemStack.is(Items.BLAST_FURNACE));
 
     public static final RegistryObject<CompartmentType<SmokerCompartmentEntity>> SMOKER_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_smoker",
             CompartmentType.Builder.of(SmokerCompartmentEntity::new, SmokerCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.SMOKER.asItem()));
+            itemStack -> itemStack.is(Items.SMOKER));
 
     public static final RegistryObject<CompartmentType<BrewingStandCompartmentEntity>> BREWING_STAND_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_brewing_stand",
             CompartmentType.Builder.of(BrewingStandCompartmentEntity::new, BrewingStandCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.BREWING_STAND.asItem()));
+            itemStack -> itemStack.is(Items.BREWING_STAND));
 
     public static final RegistryObject<CompartmentType<CraftingTableCompartment>> WORKBENCH_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_crafting_table",
@@ -101,26 +101,26 @@ public final class AlekiShipsEntities {
     public static final RegistryObject<CompartmentType<StonecutterCompartmentEntity>> STONECUTTER_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_stonecutter",
             CompartmentType.Builder.of(StonecutterCompartmentEntity::new, StonecutterCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.STONECUTTER.asItem()));
+            itemStack -> itemStack.is(Items.STONECUTTER));
 
     public static final RegistryObject<CompartmentType<CartographyTableCompartmentEntity>> CARTOGRAPHY_TABLE_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_cartography_table",
             CompartmentType.Builder.of(CartographyTableCompartmentEntity::new, CartographyTableCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.CARTOGRAPHY_TABLE.asItem()));
+            itemStack -> itemStack.is(Items.CARTOGRAPHY_TABLE));
 
     public static final RegistryObject<CompartmentType<SmithingTableCompartmentEntity>> SMITHING_TABLE_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_smithing_table",
             CompartmentType.Builder.of(SmithingTableCompartmentEntity::new, SmithingTableCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.SMITHING_TABLE.asItem()));
+            itemStack -> itemStack.is(Items.SMITHING_TABLE));
 
     public static final RegistryObject<CompartmentType<GrindstoneCompartmentEntity>> GRINDSTONE_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_grindstone",
             CompartmentType.Builder.of(GrindstoneCompartmentEntity::new, GrindstoneCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.GRINDSTONE.asItem()));
+            itemStack -> itemStack.is(Items.GRINDSTONE));
 
     public static final RegistryObject<CompartmentType<LoomCompartmentEntity>> LOOM_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_loom", CompartmentType.Builder.of(LoomCompartmentEntity::new, LoomCompartmentEntity::new),
-            itemStack -> itemStack.is(Blocks.LOOM.asItem()));
+            itemStack -> itemStack.is(Items.LOOM));
 
     public static final RegistryObject<EntityType<BoatVehiclePart>> BOAT_VEHICLE_PART = register("vehicle_part_boat",
             EntityType.Builder.of(BoatVehiclePart::new, MobCategory.MISC).sized(0, 0)
