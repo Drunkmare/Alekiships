83b0f6936c91a32214bf4c45e8ce1660029c952c
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-03-10 21:28:13 -0700

    Format file

--- src/main/java/com/alekiponi/alekiships/common/entity/CannonballEntity.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/CannonballEntity.java
@@ -26,8 +26,8 @@ public class CannonballEntity extends Entity {
     protected double lerpYRot;
     protected double lerpXRot;
 
-    public CannonballEntity(EntityType<?> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
+    public CannonballEntity(final EntityType<? extends CannonballEntity> entityType, final Level level) {
+        super(entityType, level);
     }
 
     @Override
@@ -48,43 +48,40 @@ public class CannonballEntity extends Entity {
     @Override
     public void tick() {
         super.tick();
-        this.setDeltaMovement(this.getDeltaMovement().add(0.0D, -0.02D, 0.0D));
-        if (this.isInWater() || this.level().getFluidState(this.blockPosition())
-                .is(TFCFluids.SALT_WATER.getSource())) {
-            this.setDeltaMovement(0.0D, -0.05D, 0.0D);
-            this.setYRot(this.getYRot() + 0.4f);
+        this.setDeltaMovement(this.getDeltaMovement().add(0, -0.02, 0));
+        if (this.isInWater() || this.level().getFluidState(this.blockPosition()).is(TFCFluids.SALT_WATER.getSource())) {
+            this.setDeltaMovement(0, -0.05, 0);
+            this.setYRot(this.getYRot() + 0.4F);
         }
         if (!this.onGround() || this.getDeltaMovement().horizontalDistanceSqr() > (double) 1.0E-5F) {
             this.move(MoverType.SELF, this.getDeltaMovement());
-            float f1 = 0.98F;
+            final float f1 = 0.98F;
 
             this.setDeltaMovement(this.getDeltaMovement().multiply(f1, 0.98D, f1));
             if (this.onGround()) {
-                Vec3 vec31 = this.getDeltaMovement();
-                if (vec31.y < 0.0D) {
-                    this.setDeltaMovement(vec31.multiply(1.0D, -0.5D, 1.0D));
+                final Vec3 vec31 = this.getDeltaMovement();
+                if (vec31.y < 0) {
+                    this.setDeltaMovement(vec31.multiply(1, -0.5, 1));
                 }
             }
         }
         //this.checkInsideBlocks();
-        if(tickCount > 0){
+        if (tickCount > 0) {
             // hit entities
-            List<Entity> entities = this.level()
+            final List<Entity> entities = this.level()
                     .getEntities(this, this.getBoundingBox().inflate(1.5, 1.5, 1.5), EntitySelector.NO_SPECTATORS);
 
-            if(entities.contains(this)){
-                entities.remove(this);
-            }
+            entities.remove(this);
 
-            if(entities.size() > 0){
+            if (!entities.isEmpty()) {
                 this.discard();
-                this.explode(Mth.clamp((float) Math.ceil(this.getDeltaMovement().length()/2), 1, 10));
-                for(Entity entity : entities){
-                    if(!(entity instanceof CannonEntity) && !(entity instanceof EmptyCompartmentEntity) && !(entity instanceof CannonballEntity)){
-                        if(entity instanceof AbstractAlekiBoatEntity){
+                this.explode(Mth.clamp((float) Math.ceil(this.getDeltaMovement().length() / 2), 1, 10));
+                for (final Entity entity : entities) {
+                    if (!(entity instanceof CannonEntity) && !(entity instanceof EmptyCompartmentEntity) && !(entity instanceof CannonballEntity)) {
+                        if (entity instanceof AbstractAlekiBoatEntity) {
                             entity.hurt(this.damageSources().explosion(this, entity), 100);
                         }
-                        if(entity instanceof Boat){
+                        if (entity instanceof Boat) {
                             entity.hurt(this.damageSources().explosion(this, entity), 1000);
                         }
                     }
@@ -93,50 +90,38 @@ public class CannonballEntity extends Entity {
             }
 
             //velocity based
-            if(this.getDeltaMovement().multiply(1,0,1).length()<0.1 || Math.abs(this.getDeltaMovement().y) == 0){
+            if (this.getDeltaMovement().multiply(1, 0, 1).length() < 0.1 || Math.abs(this.getDeltaMovement().y) == 0) {
                 this.discard();
-                this.explode(Mth.clamp((float) 4, 1, 10));
+                this.explode(4);
 
                 return;
             }
         }
 
-        this.level().addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, this.getX(), this.getY() + 0.5D, this.getZ(), 0.0D, 0.0D, 0.0D);
+        this.level()
+                .addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, this.getX(), this.getY() + 0.5D, this.getZ(), 0, 0,
+                        0);
         //this.tickLerp();
 
         this.updateInWaterStateAndDoFluidPushing();
     }
 
-    protected void explode(float radius) {
-        this.level().explode(this, this.getX(), this.getY(0.0625D), this.getZ(), radius, Level.ExplosionInteraction.TNT);
+    protected void explode(final float radius) {
+        this.level()
+                .explode(this, this.getX(), this.getY(0.0625D), this.getZ(), radius, Level.ExplosionInteraction.TNT);
     }
 
     @Override
     public AABB getBoundingBoxForCulling() {
         float bbRadius = 2;
-        Vec3 startingPoint = new Vec3(this.getX() - bbRadius, this.getY() - bbRadius, this.getZ() - bbRadius);
-        Vec3 endingPoint = new Vec3(this.getX() + bbRadius, this.getY() + bbRadius, this.getZ() + bbRadius);
+        final Vec3 startingPoint = new Vec3(this.getX() - bbRadius, this.getY() - bbRadius, this.getZ() - bbRadius);
+        final Vec3 endingPoint = new Vec3(this.getX() + bbRadius, this.getY() + bbRadius, this.getZ() + bbRadius);
         return new AABB(startingPoint, endingPoint);
-    }
-
-    protected void tickLerp() {
-        if (this.isControlledByLocalInstance()) {
-            this.lerpSteps = 0;
-            this.syncPacketPositionCodec(this.getX(), this.getY(), this.getZ());
-        }
-
-        if (this.lerpSteps > 0) {
-            double d0 = this.getX() + (this.lerpX - this.getX()) / (double) this.lerpSteps;
-            double d1 = this.getY() + (this.lerpY - this.getY()) / (double) this.lerpSteps;
-            double d2 = this.getZ() + (this.lerpZ - this.getZ()) / (double) this.lerpSteps;
-            --this.lerpSteps;
-            this.setPos(d0, d1, d2);
-        }
     }
 
     @Override
     public void lerpTo(final double posX, final double posY, final double posZ, final float yaw, final float pitch,
-                       final int pPosRotationIncrements, final boolean teleport) {
+            final int pPosRotationIncrements, final boolean teleport) {
         this.lerpX = posX;
         this.lerpY = posY;
         this.lerpZ = posZ;
@@ -144,5 +129,4 @@ public class CannonballEntity extends Entity {
         this.lerpXRot = pitch;
         this.lerpSteps = 10;
     }
-
 }
