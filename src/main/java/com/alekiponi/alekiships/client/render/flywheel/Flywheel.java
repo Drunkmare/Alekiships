fe99ccec809c626ec83f9f4dde1dab46027d6b79
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-04-30 00:59:02 -0700

    Icky commit of what I done diddly did so aleki can look at it if she wants while I'm at work

--- src/main/java/com/alekiponi/alekiships/client/render/flywheel/Flywheel.java
+++ src/main/java/com/alekiponi/alekiships/client/render/flywheel/Flywheel.java
@@ -1,22 +1,17 @@
 package com.alekiponi.alekiships.client.render.flywheel;
 
-import static com.jozufozu.flywheel.lib.visual.SimpleEntityVisualizer.builder;
-
 import com.alekiponi.alekiships.common.entity.AlekiShipsEntities;
-import com.alekiponi.alekiships.common.entity.vehicle.RowboatEntity;
 import com.alekiponi.alekiships.util.VanillaWood;
-import net.minecraft.world.entity.EntityType;
+
+import static com.jozufozu.flywheel.lib.visual.SimpleEntityVisualizer.builder;
 
 public class Flywheel {
 
 
-    public static void init(){
-        /*
+    public static void init() {
         for (final VanillaWood vanillaWood : VanillaWood.values()) {
-            builder(AlekiShipsEntities.ROWBOATS.get(vanillaWood).get())
-                    .factory((context, entity) -> new RowboatVisual<>(context, entity, vanillaWood));
-        }*/
-
+            builder(AlekiShipsEntities.ROWBOATS.get(vanillaWood).get()).factory(RowboatVisual::new).apply();
+        }
     }
 
 }
