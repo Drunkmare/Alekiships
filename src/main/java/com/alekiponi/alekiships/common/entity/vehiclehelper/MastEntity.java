a66fbb44fc39d6f3e9704b50351a53afe2f58f25
alekiponi <hyperdashpony@gmail.com>
2024-05-13 18:15:31 -0700

    add banner item to mast entities

--- src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/MastEntity.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/MastEntity.java
@@ -1,12 +1,24 @@
 package com.alekiponi.alekiships.common.entity.vehiclehelper;
 
+import com.alekiponi.alekiships.common.entity.vehicle.AbstractVehicle;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BannerItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.Tags;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,26 +28,14 @@ public class MastEntity extends AbstractPassthroughHelper {
         super(pEntityType, pLevel);
     }
 
+    protected static final EntityDataAccessor<ItemStack> DATA_ID_BANNER = SynchedEntityData.defineId(
+            MastEntity.class, EntityDataSerializers.ITEM_STACK);
+
     @Override
     public void tick(){
         super.tick();
 
         if(this.level().isClientSide()){
-            /*
-            final List<Entity> playersToAllowClimbing = this.level()
-                    .getEntities(this, this.getBoundingBox().inflate(0.0, 0.0, 0.0), EntitySelector.pushableBy(this));
-
-            if (!playersToAllowClimbing.isEmpty()) {
-                for (final Entity entity : playersToAllowClimbing) {
-                    if (entity instanceof LocalPlayer player) {
-                        if (player.input.jumping) {
-                            player.setPos(player.getPosition(0).add(0,0.1,0));
-                            //SoundEvents.LADDER_STEP
-                        }
-                    }
-                }
-            }*/
-
             List<net.minecraft.world.entity.Entity> playersToMoveWithMast = new ArrayList<Entity>();
 
             playersToMoveWithMast.addAll(this.level()
@@ -60,17 +60,41 @@ public class MastEntity extends AbstractPassthroughHelper {
     }
 
     @Override
-    protected void defineSynchedData() {
+    public InteractionResult interact(final Player player, final InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+        if (stack.getItem() instanceof BannerItem) {
+            this.spawnAtLocation(this.getBanner());
+            this.setBanner(stack.split(1));
+            return InteractionResult.SUCCESS;
+        }
+        if (stack.is(Tags.Items.SHEARS)) {
+            this.spawnAtLocation(this.getBanner());
+            this.setBanner(ItemStack.EMPTY);
+            return InteractionResult.SUCCESS;
+        }
+        return super.interact(player,hand);
+    }
 
+    public void setBanner(ItemStack banner){
+        entityData.set(DATA_ID_BANNER, banner);
+    }
+
+    public ItemStack getBanner(){
+        return entityData.get(DATA_ID_BANNER);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        this.entityData.define(DATA_ID_BANNER, ItemStack.EMPTY);
     }
 
     @Override
     protected void readAdditionalSaveData(CompoundTag pCompound) {
-
+        this.setBanner(ItemStack.of(pCompound.getCompound("banner")));
     }
 
     @Override
     protected void addAdditionalSaveData(CompoundTag pCompound) {
-
+        pCompound.put("banner", this.getBanner().save(new CompoundTag()));
     }
 }
