153798115815d59c32b5ac36dc1ab1d267cfbbb3
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-06-06 01:25:01 -0700

    Document vanilla (compartments) package

--- src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/package-info.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/vehiclehelper/compartment/vanilla/package-info.java
@@ -1,6 +1,14 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
+/**
+ * Everything in this package and any subpackage is related to vanilla compartments. This code is unlikely to change
+ * outside of bug fixes however in contrast to what's provided in
+ * {@link com.alekiponi.alekiships.common.entity.vehiclehelper.compartment} breaking changes may occur without warning
+ * or documentation. Certain compartment types such as
+ * {@link com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.vanilla.ChestCompartmentEntity} or
+ * {@link com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.vanilla.AbstractFurnaceCompartmentEntity}
+ * (and provided subclasses) are expected to be much more commonly used and can therefore be considered stable. Any
+ * public facing changes should be present in change logs.
+ */
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault @FieldsAreNonnullByDefault
 package com.alekiponi.alekiships.common.entity.vehiclehelper.compartment.vanilla;
 
 import net.minecraft.FieldsAreNonnullByDefault;
