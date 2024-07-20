521b42f8e9585e6c94cc2b0a87bfd0da38df4305
alekiponi <hyperdashpony@gmail.com>
2024-07-20 10:58:21 -0700

    start control backflow prevention rewrite

--- src/main/java/com/alekiponi/alekiships/network/ServerBoundSloopPacket.java
+++ src/main/java/com/alekiponi/alekiships/network/ServerBoundSloopPacket.java
@@ -2,34 +2,35 @@ package com.alekiponi.alekiships.network;
 
 import com.alekiponi.alekiships.common.entity.vehicle.SloopEntity;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ServerGamePacketListener;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.network.NetworkEvent;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
 public class ServerBoundSloopPacket {
     private final float sheetLength;
+    private final float boomRotation;
     private final float rudderAngle;
     private final int entityID;
 
-    public ServerBoundSloopPacket(float sheetLength, float rudderAngle,int entityID){
+    public ServerBoundSloopPacket(float sheetLength, float boomRotation, float rudderAngle, int entityID) {
         this.sheetLength = sheetLength;
+        this.boomRotation = boomRotation;
         this.rudderAngle = rudderAngle;
         this.entityID = entityID;
     }
 
     public ServerBoundSloopPacket(FriendlyByteBuf buffer){
         this.sheetLength = buffer.readFloat();
+        this.boomRotation = buffer.readFloat();
         this.rudderAngle = buffer.readFloat();
         this.entityID = buffer.readInt();
     }
 
     public void encoder(FriendlyByteBuf buffer){
         buffer.writeFloat(this.sheetLength);
+        buffer.writeFloat(this.boomRotation);
         buffer.writeFloat(this.rudderAngle);
         buffer.writeInt(this.entityID);
     }
@@ -42,6 +43,7 @@ public class ServerBoundSloopPacket {
                 assert player != null;
                 if (player.distanceTo(sloop) < 5) {
                     sloop.setMainsheetLength(this.sheetLength);
+                    sloop.setMainBoomRotation(this.boomRotation);
                     sloop.setRudderRotation(this.rudderAngle);
                 }
             }
