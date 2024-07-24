21e1db3616ed8aa194f770a8afe99157918b18f1
alekiponi <hyperdashpony@gmail.com>
2024-07-23 17:59:19 -0700

    version bump, make boats always bouyant near the surface

--- src/main/java/com/alekiponi/alekiships/common/entity/vehicle/AbstractAlekiBoatEntity.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/vehicle/AbstractAlekiBoatEntity.java
@@ -110,6 +110,7 @@ public abstract class AbstractAlekiBoatEntity extends AbstractVehicle {
 
         if ((this.status == MediumStatus.UNDER_WATER) && this.isFunctional() && this.tickCount % 20 == 0) {
             this.hurt(this.damageSources().drown(), this.getDamageRecovery());
+
         }
 
         if (everyNthTickUnique(5)) {
@@ -287,6 +288,9 @@ public abstract class AbstractAlekiBoatEntity extends AbstractVehicle {
                 this.setDeltaMovement(movement.x, (movement.y + d2 * 0.06153846016296973D) * 0.75D, movement.z);
             }
 
+            if (status == MediumStatus.UNDER_WATER && this.level().getFluidState(this.blockPosition().above(3)).isEmpty()) {
+                this.setDeltaMovement(getDeltaMovement().x, 1 / 20.0, getDeltaMovement().z);
+            }
 
         }
 
