f00593a8af644f8b2e05a367a9391a406d14e5fd
alekiponi <alekiponi@gmail.com>
2024-08-27 14:38:58 -0700

    re-add held models without EHM

--- src/main/java/com/alekiponi/alekiships/data/providers/models/AlekiShipsItemModelProvider.java
+++ src/main/java/com/alekiponi/alekiships/data/providers/models/AlekiShipsItemModelProvider.java
@@ -1,10 +1,18 @@
 package com.alekiponi.alekiships.data.providers.models;
 
+import java.util.Objects;
 import com.alekiponi.alekiships.AlekiShips;
 import com.alekiponi.alekiships.common.item.AlekiShipsItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.loaders.SeparateTransformsModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class AlekiShipsItemModelProvider extends ItemModelProvider {
 
@@ -14,11 +22,38 @@ public class AlekiShipsItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        this.basicItem(AlekiShipsItems.CANNON.get());
+        //this.heldItem(AlekiShipsItems.CANNON.get());
         this.basicItem(AlekiShipsItems.CANNONBALL.get());
-        this.basicItem(AlekiShipsItems.ANCHOR.get());
+        //this.heldItem(AlekiShipsItems.ANCHOR.get());
         this.basicItem(AlekiShipsItems.SLOOP_ICON_ONLY.get());
         this.basicItem(AlekiShipsItems.ROWBOAT_ICON_ONLY.get());
-        this.basicItem(AlekiShipsItems.OAR.get());
+        //this.heldItem(AlekiShipsItems.OAR.get());
+    }
+
+    public ItemModelBuilder heldItem(Item item)
+    {
+        return heldItem(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)));
+    }
+
+    public ItemModelBuilder itemNoTexture(ResourceLocation item)
+    {
+        return getBuilder(item.toString())
+            .parent(new ModelFile.UncheckedModelFile("item/generated"));
+    }
+
+    public ItemModelBuilder heldItem(ResourceLocation item)
+    {
+        return SeparateTransformsModelBuilder.begin(this.itemNoTexture(new ResourceLocation(item.getNamespace(), "held/" + item.getPath())), existingFileHelper)
+            .base(getBuilder(item.toString())
+                .parent(new ModelFile.UncheckedModelFile("held/")))
+            .perspective(ItemDisplayContext.FIXED, iconItem(item))
+            .perspective(ItemDisplayContext.GROUND, iconItem(item))
+            .perspective(ItemDisplayContext.GUI, iconItem(item)).end();
+    }
+
+    public ItemModelBuilder iconItem(ResourceLocation item)
+    {
+        return getBuilder(item.toString())
+            .parent(this.itemNoTexture(new ResourceLocation(item.getNamespace(), "icon/" + item.getPath())));
     }
 }
