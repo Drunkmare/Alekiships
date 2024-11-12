9f6d1afa45e21703b22f45d3f52cce154aef1cba
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-11-12 00:14:15 -0800

    Kotlin build file for Neo 1.21

---
+++ build.gradle.kts
@@ -0,0 +1,188 @@
+import org.slf4j.event.Level
+
+plugins {
+    idea
+    id("net.neoforged.moddev") version "2.0.1-beta"
+}
+
+// Mappings
+val parchmentVersion: String = "2024.07.07"
+val parchmentMinecraftVersion: String = "1.21"
+
+//# Mod stuff
+val modID: String = "alekiships"
+val modName: String = "aleki's Nifty Ships"
+val modVersion: String = "1.0.0"
+//# Minecraft stuff
+val minecraftVersion: String = "1.21"
+val minecraftVersionRange: String = "[1.21,1.21.1)"
+val neoVersion: String = "21.0.167"
+val neoVersionRange: String = "[21.0.167,21.0.168 )"
+
+// Dependency versions
+val emiVersion: String = "1.1.10+1.21"
+val jeiVersion: String = "19.5.2.66"
+val topVersion: String = "1.21_neo-12.0.4-6"
+val jadeFileID: String = "4614153"
+
+val datagenOutput: String = "src/generated/resources"
+
+val generateModMetadata = tasks.register<ProcessResources>("generateModMetadata") {
+    val modReplacementProperties = mapOf(
+        "modId" to modID,
+        "modName" to modName,
+        "modVersion" to modVersion,
+        "minecraftVersionRange" to "[$minecraftVersion,)",
+        "neoForgeVersionRange" to "[$neoVersion,)",
+        "jeiVersionRange" to "[$jeiVersion,)"
+    )
+    inputs.properties(modReplacementProperties)
+    expand(modReplacementProperties)
+    from("src/main/templates")
+    into(layout.buildDirectory.dir("generated/sources/modMetadata"))
+}
+
+base {
+    archivesName.set("alekiNiftyShips-FORGE-$minecraftVersion")
+    version = modVersion
+    group = modID
+}
+
+// Mojang ships Java 21 to end users starting in 1.20.5, so mods should target Java 21.
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+sourceSets {
+    main {
+        resources {
+            srcDir(datagenOutput)
+            srcDir(generateModMetadata)
+        }
+    }
+    create("datagen")
+}
+
+configurations {
+    // Sets up a dependency configuration called 'localRuntime'.
+    // This configuration should be used instead of 'runtimeOnly' to declare
+    // a dependency that will be present for runtime testing but that is
+    // "optional", meaning it will not be pulled by dependents of this mod.
+    get("runtimeClasspath").extendsFrom(create("localRuntime"))
+    // Datagen can reference our code in main
+    get("datagenCompileClasspath").extendsFrom(compileClasspath.get())
+    get("datagenRuntimeClasspath").extendsFrom(runtimeClasspath.get())
+}
+
+neoForge {
+    version.set(neoVersion)
+    addModdingDependenciesTo(sourceSets["datagen"])
+    validateAccessTransformers = true
+
+    parchment {
+        minecraftVersion.set(parchmentMinecraftVersion)
+        mappingsVersion.set(parchmentVersion)
+    }
+
+    runs {
+        configureEach {
+            systemProperty("forge.logging.markers", "REGISTRIES")
+            logLevel = Level.INFO
+            systemProperty("forge.enabledGameTestNamespaces", modID)
+
+            // Only JBR allows enhanced class redefinition, so ignore the option for any other JDKs
+            jvmArguments.addAll("-XX:+IgnoreUnrecognizedVMOptions", "-XX:+AllowEnhancedClassRedefinition", "-ea")
+        }
+
+        register("client") {
+            client()
+        }
+
+        // Second client run for dev 2 player testing
+        register("client2") {
+            client()
+            programArguments.addAll("--username", "Dev2")
+        }
+
+        register("server") {
+            server()
+            programArgument("--nogui")
+        }
+
+        register("datagen") {
+            data()
+
+            // Specify the modid for data generation, where to output the resulting resource, and where to look for existing resources.
+            programArguments.addAll(
+                "--mod",
+                modID,
+                "--all",
+                "--output",
+                file(datagenOutput).absolutePath,
+                "--existing",
+                file("src/main/resources/").absolutePath
+            )
+        }
+    }
+
+
+    mods {
+        create(modID) {
+            sourceSet(sourceSets.main.get())
+            sourceSet(sourceSets["datagen"])
+        }
+    }
+
+    ideSyncTask(generateModMetadata)
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    exclusiveContent {
+        forRepository { maven { url = uri("https://maven.terraformersmc.com/") } }
+        filter { includeGroup("dev.emi") }
+    }
+    exclusiveContent {
+        forRepository { maven { url = uri("https://maven.blamejared.com/") } }
+        filter { includeGroup("mezz.jei") }
+    }
+    exclusiveContent {
+        forRepository { maven { url = uri("https://maven.k-4u.nl/") } }
+        filter { includeGroup("mcjty.theoneprobe") }
+    }
+    exclusiveContent {
+        forRepository { maven { url = uri("https://www.cursemaven.com") } }
+        filter { includeGroup("curse.maven") }
+    }
+}
+
+dependencies {
+    "datagenImplementation"(sourceSets["main"].output)
+
+    // QOL Dev dependencies should use `localRuntime`. `runtimeOnly` is for stuff we actually want at runtime
+
+    implementation("curse.maven:jade-324717:$jadeFileID")
+    compileOnly("mcjty.theoneprobe:theoneprobe:$topVersion")
+
+    // EMI
+    compileOnly("dev.emi:emi-neoforge:${emiVersion}:api")
+    //runtimeOnly("dev.emi:emi-neoforge:${emiVersion}")
+
+    // JEI
+    compileOnly("mezz.jei:jei-${minecraftVersion}-common-api:${jeiVersion}")
+    compileOnly("mezz.jei:jei-${minecraftVersion}-neoforge-api:${jeiVersion}")
+    runtimeOnly("mezz.jei:jei-${minecraftVersion}-neoforge:${jeiVersion}")
+}
+
+// IDEA no longer automatically downloads sources/javadoc jars for dependencies, so we need to explicitly enable the behavior.
+idea {
+    module {
+        isDownloadSources = true
+        isDownloadJavadoc = true
+    }
+}
+
+tasks {
+    named("neoForgeIdeSync") {
+        dependsOn(generateModMetadata)
+    }
+}
