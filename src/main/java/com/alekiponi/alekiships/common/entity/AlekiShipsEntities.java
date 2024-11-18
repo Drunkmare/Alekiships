60fe9a55937c8f7f6c8957e0ecbd4f3bb2524157
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-11-18 14:17:18 -0800

    Whoops. Fields should be `DeferredHolder<EntityType<?>, EntityType<E>>`

--- src/main/java/com/alekiponi/alekiships/common/entity/AlekiShipsEntities.java
+++ src/main/java/com/alekiponi/alekiships/common/entity/AlekiShipsEntities.java
@@ -34,145 +34,145 @@ public final class AlekiShipsEntities {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(
             BuiltInRegistries.ENTITY_TYPE, MOD_ID);
 
-    public static final EnumMap<VanillaWood, Supplier<EntityType<RowboatEntity>>> ROWBOATS = CommonHelper.mapOfKeys(
+    public static final EnumMap<VanillaWood, DeferredHolder<EntityType<?>, EntityType<RowboatEntity>>> ROWBOATS = CommonHelper.mapOfKeys(
             VanillaWood.class, vanillaWood -> registerRowboat(vanillaWood,
                     EntityType.Builder.of((entityType, level) -> new RowboatEntity(entityType, level, vanillaWood),
                             MobCategory.MISC)));
 
-    public static final EnumMap<VanillaWood, Supplier<EntityType<SloopEntity>>> SLOOPS = CommonHelper.mapOfKeys(
+    public static final EnumMap<VanillaWood, DeferredHolder<EntityType<?>, EntityType<SloopEntity>>> SLOOPS = CommonHelper.mapOfKeys(
             VanillaWood.class, vanillaWood -> registerSloop(vanillaWood,
                     EntityType.Builder.of((entityType, level) -> new SloopEntity(entityType, level, vanillaWood),
                             MobCategory.MISC)));
 
-    public static final EnumMap<VanillaWood, Supplier<EntityType<SloopUnderConstructionEntity>>> SLOOPS_UNDER_CONSTRUCTION = CommonHelper.mapOfKeys(
+    public static final EnumMap<VanillaWood, DeferredHolder<EntityType<?>, EntityType<SloopUnderConstructionEntity>>> SLOOPS_UNDER_CONSTRUCTION = CommonHelper.mapOfKeys(
             VanillaWood.class, vanillaWood -> registerSloopConstruction(vanillaWood, EntityType.Builder.of(
                     (entityType, level) -> new SloopUnderConstructionEntity(entityType, level, vanillaWood),
                     MobCategory.MISC)));
 
-    public static final Supplier<CompartmentType<EmptyCompartmentEntity>> EMPTY_COMPARTMENT_ENTITY = registerCompartment(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<EmptyCompartmentEntity>> EMPTY_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_empty", CompartmentType.Builder.createBasic(EmptyCompartmentEntity::new));
 
-    public static final Supplier<CompartmentType<BlockCompartmentEntity>> BLOCK_COMPARTMENT_ENTITY = registerCompartment(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<BlockCompartmentEntity>> BLOCK_COMPARTMENT_ENTITY = registerCompartment(
             "compartment_block", CompartmentType.Builder.of(BlockCompartmentEntity::new, BlockCompartmentEntity::new));
 
-    public static final Supplier<CompartmentType<BarrelCompartmentEntity>> BARREL_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<BarrelCompartmentEntity>> BARREL_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_barrel",
                     CompartmentType.Builder.of(BarrelCompartmentEntity::new, BarrelCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.BARREL));
 
-    public static final Supplier<CompartmentType<ChestCompartmentEntity>> CHEST_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<ChestCompartmentEntity>> CHEST_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_chest",
                     CompartmentType.Builder.of(ChestCompartmentEntity::new, ChestCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.CHEST));
 
-    public static final Supplier<CompartmentType<EnderChestCompartmentEntity>> ENDER_CHEST_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<EnderChestCompartmentEntity>> ENDER_CHEST_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_ender_chest",
                     CompartmentType.Builder.createBasic(EnderChestCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.ENDER_CHEST));
 
-    public static final Supplier<CompartmentType<ShulkerBoxCompartmentEntity>> SHULKER_BOX_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<ShulkerBoxCompartmentEntity>> SHULKER_BOX_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_shulker_box",
                     CompartmentType.Builder.of(ShulkerBoxCompartmentEntity::new, ShulkerBoxCompartmentEntity::new)),
             itemStack -> itemStack.is(AlekiShipsTags.Items.SHULKER_BOXES));
 
-    public static final Supplier<CompartmentType<FurnaceCompartmentEntity>> FURNACE_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<FurnaceCompartmentEntity>> FURNACE_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_furnace",
                     CompartmentType.Builder.of(FurnaceCompartmentEntity::new, FurnaceCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.FURNACE));
 
-    public static final Supplier<CompartmentType<BlastFurnaceCompartmentEntity>> BLAST_FURNACE_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<BlastFurnaceCompartmentEntity>> BLAST_FURNACE_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_blast_furnace",
                     CompartmentType.Builder.of(BlastFurnaceCompartmentEntity::new, BlastFurnaceCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.BLAST_FURNACE));
 
-    public static final Supplier<CompartmentType<SmokerCompartmentEntity>> SMOKER_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<SmokerCompartmentEntity>> SMOKER_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_smoker",
                     CompartmentType.Builder.of(SmokerCompartmentEntity::new, SmokerCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.SMOKER));
 
-    public static final Supplier<CompartmentType<BrewingStandCompartmentEntity>> BREWING_STAND_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<BrewingStandCompartmentEntity>> BREWING_STAND_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_brewing_stand",
                     CompartmentType.Builder.of(BrewingStandCompartmentEntity::new, BrewingStandCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.BREWING_STAND));
 
-    public static final Supplier<CompartmentType<CraftingTableCompartment>> WORKBENCH_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<CraftingTableCompartment>> WORKBENCH_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_crafting_table",
                     CompartmentType.Builder.of(CraftingTableCompartment::new, CraftingTableCompartment::new)),
             itemStack -> itemStack.is(AlekiShipsTags.Items.CRAFTING_TABLES));
 
-    public static final Supplier<CompartmentType<StonecutterCompartmentEntity>> STONECUTTER_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<StonecutterCompartmentEntity>> STONECUTTER_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_stonecutter",
                     CompartmentType.Builder.of(StonecutterCompartmentEntity::new, StonecutterCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.STONECUTTER));
 
-    public static final Supplier<CompartmentType<CartographyTableCompartmentEntity>> CARTOGRAPHY_TABLE_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<CartographyTableCompartmentEntity>> CARTOGRAPHY_TABLE_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_cartography_table",
                     CompartmentType.Builder.of(CartographyTableCompartmentEntity::new,
                             CartographyTableCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.CARTOGRAPHY_TABLE));
 
-    public static final Supplier<CompartmentType<SmithingTableCompartmentEntity>> SMITHING_TABLE_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<SmithingTableCompartmentEntity>> SMITHING_TABLE_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_smithing_table",
                     CompartmentType.Builder.of(SmithingTableCompartmentEntity::new,
                             SmithingTableCompartmentEntity::new)), itemStack -> itemStack.is(Items.SMITHING_TABLE));
 
-    public static final Supplier<CompartmentType<GrindstoneCompartmentEntity>> GRINDSTONE_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<GrindstoneCompartmentEntity>> GRINDSTONE_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_grindstone",
                     CompartmentType.Builder.of(GrindstoneCompartmentEntity::new, GrindstoneCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.GRINDSTONE));
 
-    public static final Supplier<CompartmentType<LoomCompartmentEntity>> LOOM_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<LoomCompartmentEntity>> LOOM_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_loom",
                     CompartmentType.Builder.of(LoomCompartmentEntity::new, LoomCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.LOOM));
 
-    public static final Supplier<CompartmentType<NoteBlockCompartmentEntity>> NOTE_BLOCK_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<NoteBlockCompartmentEntity>> NOTE_BLOCK_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_note_block",
                     CompartmentType.Builder.of(NoteBlockCompartmentEntity::new, NoteBlockCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.NOTE_BLOCK));
 
-    public static final Supplier<CompartmentType<JukeboxCompartmentEntity>> JUKEBOX_COMPARTMENT_ENTITY = CompartmentType.register(
+    public static final DeferredHolder<EntityType<?>, CompartmentType<JukeboxCompartmentEntity>> JUKEBOX_COMPARTMENT_ENTITY = CompartmentType.register(
             registerCompartment("compartment_jukebox",
                     CompartmentType.Builder.of(JukeboxCompartmentEntity::new, JukeboxCompartmentEntity::new)),
             itemStack -> itemStack.is(Items.JUKEBOX));
 
-    public static final Supplier<EntityType<VehiclePart>> VEHICLE_PART = register("vehicle_part",
+    public static final DeferredHolder<EntityType<?>, EntityType<VehiclePart>> VEHICLE_PART = register("vehicle_part",
             EntityType.Builder.of(VehiclePart::new, MobCategory.MISC).sized(0, 0)
                     .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<CleatEntity>> VEHICLE_CLEAT_ENTITY = register("vehicle_cleat",
+    public static final DeferredHolder<EntityType<?>, EntityType<CleatEntity>> VEHICLE_CLEAT_ENTITY = register("vehicle_cleat",
             EntityType.Builder.of(CleatEntity::new, MobCategory.MISC).sized(0.4F, 0.2F)
                     .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<ColliderEntity>> VEHICLE_COLLIDER_ENTITY = register(
+    public static final DeferredHolder<EntityType<?>, EntityType<ColliderEntity>> VEHICLE_COLLIDER_ENTITY = register(
             "vehicle_collider",
             EntityType.Builder.of(ColliderEntity::new, MobCategory.MISC).sized(1, 1).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<SailSwitchEntity>> SAIL_SWITCH_ENTITY = register(
+    public static final DeferredHolder<EntityType<?>, EntityType<SailSwitchEntity>> SAIL_SWITCH_ENTITY = register(
             "vehicle_switch_sail",
             EntityType.Builder.of(SailSwitchEntity::new, MobCategory.MISC).sized(0.8F, 0.8F).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<AnchorEntity>> ANCHOR_ENTITY = register("vehicle_anchor",
+    public static final DeferredHolder<EntityType<?>, EntityType<AnchorEntity>> ANCHOR_ENTITY = register("vehicle_anchor",
             EntityType.Builder.of(AnchorEntity::new, MobCategory.MISC).sized(1, 1)
                     .clientTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<ConstructionEntity>> CONSTRUCTION_ENTITY = register(
+    public static final DeferredHolder<EntityType<?>, EntityType<ConstructionEntity>> CONSTRUCTION_ENTITY = register(
             "vehicle_construction", EntityType.Builder.of(ConstructionEntity::new, MobCategory.MISC).sized(1, 1)
                     .clientTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<WindlassSwitchEntity>> WINDLASS_SWITCH_ENTITY = register(
+    public static final DeferredHolder<EntityType<?>, EntityType<WindlassSwitchEntity>> WINDLASS_SWITCH_ENTITY = register(
             "vehicle_switch_windlass",
             EntityType.Builder.of(WindlassSwitchEntity::new, MobCategory.MISC).sized(0.8F, 0.8F)
                     .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
-    public static final Supplier<EntityType<CannonballEntity>> CANNONBALL_ENTITY = register("cannonball",
+    public static final DeferredHolder<EntityType<?>, EntityType<CannonballEntity>> CANNONBALL_ENTITY = register("cannonball",
             EntityType.Builder.<CannonballEntity>of(CannonballEntity::new, MobCategory.MISC).sized(0.5F, 0.5F)
                     .setTrackingRange(32).clientTrackingRange(32).setShouldReceiveVelocityUpdates(true).fireImmune());
 
-    public static final Supplier<EntityType<CannonEntity>> CANNON_ENTITY = register("cannon",
+    public static final DeferredHolder<EntityType<?>, EntityType<CannonEntity>> CANNON_ENTITY = register("cannon",
             EntityType.Builder.of(CannonEntity::new, MobCategory.MISC).sized(0.8F, 0.8F).fireImmune());
 
-    public static final Supplier<EntityType<MastEntity>> MAST_ENTITY = register("vehicle_mast",
+    public static final DeferredHolder<EntityType<?>, EntityType<MastEntity>> MAST_ENTITY = register("vehicle_mast",
             EntityType.Builder.of(MastEntity::new, MobCategory.MISC).sized(0.3F, 8.79375f)
                     .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon().fireImmune());
 
