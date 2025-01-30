b75597834c1ab7342ff99c5ad291732df23d0674
Traister101 <66151907+Traister101@users.noreply.github.com>
2025-01-29 20:35:13 -0800

    Remove old compartment item tags

--- src/datagen/java/com/alekiponi/alekiships/data/providers/tags/AlekiShipsItemTagsProvider.java
+++ src/datagen/java/com/alekiponi/alekiships/data/providers/tags/AlekiShipsItemTagsProvider.java
@@ -6,16 +6,12 @@ import com.alekiponi.alekiships.util.AlekiShipsTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.ShulkerBoxBlock;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 public class AlekiShipsItemTagsProvider extends ItemTagsProvider {
@@ -29,22 +25,7 @@ public class AlekiShipsItemTagsProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(final HolderLookup.Provider provider) {
-        // Tag that allows things to go into compartments
-        this.tag(AlekiShipsTags.Items.CAN_PLACE_IN_COMPARTMENTS)
-                .add(Items.BARREL, Items.CHEST, Items.ENDER_CHEST, Items.FURNACE, Items.BLAST_FURNACE, Items.SMOKER,
-                        Items.STONECUTTER, Items.CARTOGRAPHY_TABLE, Items.SMITHING_TABLE, Items.GRINDSTONE, Items.LOOM,
-                        Items.BREWING_STAND, Items.NOTE_BLOCK, Items.JUKEBOX)
-                .addTag(AlekiShipsTags.Items.CRAFTING_TABLES).addTag(AlekiShipsTags.Items.SHULKER_BOXES);
-
-        this.tag(AlekiShipsTags.Items.CRAFTING_TABLES).add(Items.CRAFTING_TABLE);
-
         this.tag(Tags.Items.MUSIC_DISCS).add(AlekiShipsItems.MUSIC_DISC_PIRATE_CRAFTING.get());
-
-        {
-            final var shulkerBoxes = this.tag(AlekiShipsTags.Items.SHULKER_BOXES).add(Items.SHULKER_BOX);
-            Arrays.stream(DyeColor.values()).map(ShulkerBoxBlock::getBlockByColor).map(ItemLike::asItem)
-                    .forEach(shulkerBoxes::add);
-        }
 
         this.tag(AlekiShipsTags.Items.ICEBREAKER_UPGRADES).add(Items.IRON_BLOCK);
     }
