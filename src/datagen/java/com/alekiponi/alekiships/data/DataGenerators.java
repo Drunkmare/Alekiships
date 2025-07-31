d6dae9280b54c492bc8c6288de08ca560444661d
Traister101 <66151907+Traister101@users.noreply.github.com>
2025-07-30 23:08:05 -0700

    Generate our `pack.mcmeta`

--- src/datagen/java/com/alekiponi/alekiships/data/DataGenerators.java
+++ src/datagen/java/com/alekiponi/alekiships/data/DataGenerators.java
@@ -1,5 +1,7 @@
 package com.alekiponi.alekiships.data;
 
+import weather2.Weather;
+
 import com.alekiponi.alekiships.AlekiShips;
 import com.alekiponi.alekiships.common.AlekiShipsRegistries;
 import com.alekiponi.alekiships.common.entity.EntityInput;
@@ -12,20 +14,31 @@ import com.alekiponi.alekiships.data.providers.models.AlekiShipsItemModelProvide
 import com.alekiponi.alekiships.data.providers.tags.*;
 import com.alekiponi.alekiships.util.VanillaWood;
 
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.metadata.PackMetadataGenerator;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.packs.OverlayMetadataSection;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.util.InclusiveRange;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
+import net.neoforged.neoforge.common.conditions.WithConditions;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.common.data.GeneratingOverlayMetadataSection;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -36,6 +49,15 @@ public final class DataGenerators {
     public static void gatherData(final GatherDataEvent event) {
         final DataGenerator generator = event.getGenerator();
         final PackOutput packOutput = generator.getPackOutput();
+
+        generator.addProvider(true, new PackMetadataGenerator(packOutput))
+                .add(PackMetadataSection.TYPE,
+                        new PackMetadataSection(Component.literal("Aleki's Nifty Ships mod Resources/Data"), -1))
+                .add(GeneratingOverlayMetadataSection.NEOFORGE_TYPE, new GeneratingOverlayMetadataSection(
+                        List.of(new WithConditions<>(new OverlayMetadataSection.OverlayEntry(
+                                new InclusiveRange<>(DetectedVersion.BUILT_IN.getPackVersion(PackType.SERVER_DATA),
+                                        DetectedVersion.BUILT_IN.getPackVersion(PackType.SERVER_DATA)), Weather.MODID),
+                                new ModLoadedCondition(Weather.MODID)))));
 
         final CompletableFuture<HolderLookup.Provider> lookupProvider = generator.addProvider(event.includeServer(),
                 new DatapackBuiltinEntriesProvider(packOutput, event.getLookupProvider(), datapackEntries(),
